2da9ec0dc9182547455eca77d8ef5261b76a777c
chingari5268 <46900536+chingari5268@users.noreply.github.com>
2023-04-08 21:45:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,18 @@ variable "agencies" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "${var.agencies}-bucket"
-  acl    = "private"
   
   tags = {
     Name        = "My Example Bucket"
     Environment = "Production"
   }
+}
+
+# Set the ACL for the S3 bucket
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  # Set the ACL to private
+  # See https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl for more options
+  acl = "private"
 }
